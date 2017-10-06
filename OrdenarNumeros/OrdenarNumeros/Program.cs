2e037fcc6fcc8d6e9be04161224a94ee4498d44c
Joel Lopez <jallende153@gmail.com>
2017-10-06 18:49:37 -0500

    primera versión

--- OrdenarNumeros/OrdenarNumeros/Program.cs
+++ OrdenarNumeros/OrdenarNumeros/Program.cs
@@ -10,6 +10,59 @@ namespace OrdenarNumeros
     {
         static void Main(string[] args)
         {
+            int num1, num2, num3;
+            Console.Write("Ingrese el Primer Número: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese el Segundo Número: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese el Tercer Número: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            if(num1> num2 && num1 > num3)
+            {
+                Console.WriteLine("El número mayor es: {0} ", num1);
+                if(num2 > num3)
+                {
+                    Console.WriteLine("El número del medio es: {0}", num2);
+                    Console.WriteLine("El número menor es: {0}", num3);
+                }
+                else
+                {
+                    Console.WriteLine("El número del medio es: {0}", num3);
+                    Console.WriteLine("El número menor es: {0}", num2);
+                }
+            }
+            else
+            {
+                if(num2 > num1 && num2 > num3)
+                {
+                    Console.WriteLine("El número mayor es: {0} ", num2);
+                    if (num1 > num3)
+                    {
+                        Console.WriteLine("El número del medio es: {0}", num1);
+                        Console.WriteLine("El número menor es: {0}", num3);
+                    }
+                    else
+                    {
+                        Console.WriteLine("El número del medio es: {0}", num3);
+                        Console.WriteLine("El número menor es: {0}", num1);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("El número mayor es: {0} ", num3);
+                    if (num1 > num2)
+                    {
+                        Console.WriteLine("El número del medio es: {0}", num1);
+                        Console.WriteLine("El número menor es: {0}", num2);
+                    }
+                    else
+                    {
+                        Console.WriteLine("El número del medio es: {0}", num2);
+                        Console.WriteLine("El número menor es: {0}", num1);
+                    }
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
